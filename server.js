fbb2ca84e3bcac5ba59e44cdf5764ae9facfa4c8
PanisaBD <panisa2548@gmail.com>
2025-04-16 17:13:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.use('/api/v1/auth',auth);
    res.status(200).json({success:true, data:{id:1}});
  });
  app.use('/api/v1/reviews', reviews);
+ app.use('/api/v1/campgrounds/:campgroundId/reviews', reviews);
+
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 
